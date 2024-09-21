a3bc1cbe42435ea814ba037ed810bb5ca38487cc
anfractuosity <anfractuosity@anfractuosity.com>
2024-09-21 18:16:35 +0100

    Generate a heatmap for each colour channel
    Found sharpness appears different for different colour
    channels.

--- sharpness.py
+++ sharpness.py
@@ -16,29 +16,44 @@ import numpy as np
 # Found Laplacian function here:
 # https://github.com/ArduCAM/RaspberryPi/blob/master/Motorized_Focus_Camera/python/Autofocus.py#L14
 def laplacian(img):
+    b, g, r = cv2.split(img)
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img_sobel = np.abs(cv2.Laplacian(img_gray, cv2.CV_16S, ksize=1))
-    return cv2.mean(img_sobel)[0]
+    img_sobel_r = np.abs(cv2.Laplacian(r, cv2.CV_64F, ksize=1))
+    img_sobel_g = np.abs(cv2.Laplacian(g, cv2.CV_64F, ksize=1))
+    img_sobel_b = np.abs(cv2.Laplacian(b, cv2.CV_64F, ksize=1))
+    img_sobel_gray = np.abs(cv2.Laplacian(img_gray, cv2.CV_64F, ksize=1))
+    return (cv2.mean(img_sobel_r)[0], cv2.mean(img_sobel_g)[0], cv2.mean(img_sobel_b)[0], cv2.mean(img_sobel_gray)[0])
 
 
 def proc(name, tile):
+    letter = ["r", "g", "b", "gr"]
     image = cv2.imread(name)
     if image is None:
         return
+    sharplist = " ".join([f"{x[0]}: {x[1]}" for x in list(zip(letter, laplacian(image)))])
+    print(f"Average Sharpness for {name}  {sharplist}")
     height, width, _ = image.shape
-    tmp = np.zeros(((height // tile) + 1, (width // tile) + 1))
+    tmp_r = np.zeros(((height // tile) + 1, (width // tile) + 1))
+    tmp_g = np.zeros(((height // tile) + 1, (width // tile) + 1))
+    tmp_b = np.zeros(((height // tile) + 1, (width // tile) + 1))
+    tmp_gray = np.zeros(((height // tile) + 1, (width // tile) + 1))
 
     for y in range(0, height, tile):
         for x in range(0, width, tile):
             crop_img = image[y : y + tile, x : x + tile]
-            tmp[y // tile][x // tile] = laplacian(crop_img)
+            r, g, b, gray = laplacian(crop_img)
+            tmp_r[y // tile][x // tile] = r
+            tmp_g[y // tile][x // tile] = g
+            tmp_b[y // tile][x // tile] = b
+            tmp_gray[y // tile][x // tile] = gray
 
-    plt.imshow(tmp, cmap="hot", interpolation="nearest")
-    plt.colorbar()
-    namew = Path(name).with_suffix("")
-    print(f"Writing to {namew}-heatmap.png")
-    plt.savefig(f"{namew}-heatmap.png")
-    plt.close()
+    for l, tmp in zip(letter, (tmp_r, tmp_g, tmp_b, tmp_gray)):
+        plt.imshow(tmp, cmap="hot", interpolation="nearest")
+        plt.colorbar()
+        namew = Path(name).with_suffix("")
+        print(f"Writing to {namew}-{l}-heatmap.png")
+        plt.savefig(f"{namew}-{l}-heatmap.png")
+        plt.close()
 
 
 if __name__ == "__main__":
